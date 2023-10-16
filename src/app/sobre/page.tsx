2fb46e05223c2e6a597adf673abe9642957606d9
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-10-16 15:40:58 -0300

    feat: pagina sobre criada

---
+++ src/app/sobre/page.tsx
@@ -0,0 +1,10 @@
+
+const Sobre = () => {
+    return (
+        <div>
+            <h1 className="font-semibold text-3xl">Sobre</h1>
+            <p>Este é um site de receitas. Aqui você encontrará diversas refeições e pratos típicos para cozinhar, sinta-se a vontade para aprender e aprimorar seus dotes culinários!</p>
+        </div>
+    )
+}
+export default Sobre
