a63ae5d2aa293d67c5b3eeb7925677de9403c3a9
Pedro Henrique <pedrohsj.dev@gmail.com>
2023-10-19 19:51:00 -0300

    fix: update YoutubeVideo comp type

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "~/components/Navbar"
+
+
+const Categories = () => {
+    return (
+        <div>
+        <Navbar/>
+        <h1>Categorias</h1>
+        </div>
+    )
+}
+export default Categories
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,10 @@
+import Navbar from "~/components/Navbar"
 import { lobster } from "../fonts"
 
 export default function Home() {
     return (
         <div>
-            <header>
-                <div className="py-3 flex items-center justify-around">
-                    <span className={`${lobster.className} text-5xl`}>
-                        <a href="/">
-                            Tastebite
-                        </a>
-                    </span>
-                    <nav className="flex gap-6">
-                        <a className="hover:underline" href="/categorias">Categorias</a>
-                        <a className="hover:underline" href="/sobre">Sobre</a>
-                    </nav>
-                    <div className="relative">
-                        <input
-                            type="text"
-                            placeholder="Pesquisar receitas"
-                            className="border border-gray-300 rounded-full py-2 px-6 w-64 focus:outline-none focus:border-blue-700" />
-                            <img src="/search.svg" width={20} height={20} className="absolute right-0 top-0 mx-4 mt-3"/>
-                    </div>
-                </div>
-                <hr/>
-            </header>
+            <Navbar/>
             <main className="mx-auto w-fit">
                 <div className="mt-12">
                     <h1 className="text-4xl font-semibold">Receita do Dia</h1>
